98a2c39f4f5d50fc4d64d4d0755e45f15efc39de
SofiaMEweiss <sofia.weiss@iths.se>
2021-05-27 16:24:15 +0200

    Added introduction to start-comp

--- src/components/start/Start.tsx
+++ src/components/start/Start.tsx
@@ -1,6 +1,12 @@
 const Start = () => (
 	<div>
 		<h1>Välkommen till Hamsterwars</h1>
+
+		A long time ago in a galaxy far, far away....
+
+		Inleddes det stora kriget om den bästa hamstern och det pågår än idag. Det handlade inte om styrka, intelligens eller ens snabbhet. Det är helt och hållet den sötaste som kommer klättra till toppen. 
+
+		Din uppgift är att välja vem som ska gå segrande ur HAMSTER WARS!
 	</div>
 )
 

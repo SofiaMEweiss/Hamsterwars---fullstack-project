3e3a4d26ac8fc52051cedd9e9851172df4620975
SofiaMEweiss <sofia.weiss@iths.se>
2021-05-31 21:50:51 +0200

    CSS on start page finished

--- src/components/start/Start.tsx
+++ src/components/start/Start.tsx
@@ -1,29 +1,23 @@
+import { Link } from 'react-router-dom';
 import './Start.css';
 
 const Start = () => (
 	<div className="start-container">
-		
 		<section className="left-section">
-			<section className="start-content">
-		<p>THIS IS </p>
-		<h1>Hamster Wars</h1>
-
-		<p>A long time ago in a galaxy far, far away.... <br/><br/>
-
-The great battle of the greatest hamster begun.<br/>
-It was neither about strength, intelligence or even swiftness.<br/>
-Climbing to the top would simply be dependant on one thing.<br/>
-Which hamster was the cutest hamster of them all?</p>
-
-<h2>It’s all up to you Human, to decide which Hamster will prevail in Hamster Wars!</h2>
-</section>
-</section>
-<section className="right-section">
-	<button>Battle</button>
-</section>
-
-
-
+			<section>
+				<h2>This is </h2>
+				<h1>Hamster Wars</h1>
+				<p>A long time ago in a galaxy far, far away.... <br/><br/>
+				The great battle of the greatest hamster begun.<br/>
+				It was neither about strength, intelligence or even swiftness.<br/>
+				Climbing to the top would simply be dependant on one thing.<br/>
+				Which hamster was the cutest hamster of them all?</p>
+				<p className="highlight-paragraph">It’s all up to you Human, to decide which Hamster will prevail in Hamster Wars!</p>
+			</section>
+		</section>
+		<section className="right-section">
+			<Link to="/battle"><button>battle</button></Link>
+		</section>
 	</div>
 )
 

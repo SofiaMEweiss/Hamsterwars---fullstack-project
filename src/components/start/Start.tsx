87271a03360d4077b6637dfe06f708507c3e7b4c
SofiaMEweiss <sofia.weiss@iths.se>
2021-05-31 20:20:44 +0200

    Updated CSS on start page

--- src/components/start/Start.tsx
+++ src/components/start/Start.tsx
@@ -2,19 +2,28 @@ import './Start.css';
 
 const Start = () => (
 	<div className="start-container">
-		<section>
-		<p>This is </p>
+		
+		<section className="left-section">
+			<section className="start-content">
+		<p>THIS IS </p>
 		<h1>Hamster Wars</h1>
 
-		<p>A long time ago in a galaxy far, far away....
+		<p>A long time ago in a galaxy far, far away.... <br/><br/>
 
-The great battle of the greatest hamster begun.
-It was neither about strength, intelligence or even swiftness.
-Climbing to the top would simply be dependant on one thing.
+The great battle of the greatest hamster begun.<br/>
+It was neither about strength, intelligence or even swiftness.<br/>
+Climbing to the top would simply be dependant on one thing.<br/>
 Which hamster was the cutest hamster of them all?</p>
 
 <h2>It’s all up to you Human, to decide which Hamster will prevail in Hamster Wars!</h2>
 </section>
+</section>
+<section className="right-section">
+	<button>Battle</button>
+</section>
+
+
+
 	</div>
 )
 

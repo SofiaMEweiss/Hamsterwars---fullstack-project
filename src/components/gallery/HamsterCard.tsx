a9dc8e21d9cb4b8d18739dcc2ea66bdf23feba0e
SofiaMEweiss <sofia.weiss@iths.se>
2021-06-01 17:24:58 +0200

    Updated CSS in hamster card comp

--- src/components/gallery/HamsterCard.tsx
+++ src/components/gallery/HamsterCard.tsx
@@ -1,51 +1,45 @@
 import React from 'react'
-import './Gallery.css';
+import './HamsterCard.css';
 import { useEffect, useState } from 'react';
-// import { useState } from 'react';
 import HamsterInfo from './HamsterInfo';
 import { Hamster } from '../../types/Hamster'
 
-
 const HamsterCard = () => {
 	const [hamsters, setHamsters] = useState<null | Hamster[]>(null)
-	//Ändra any, funkar så länge, skapa inteface
+	
 	useEffect(() => {
-
-getHamsters()	
+		getHamsters()	
 	}, [])
 
 	async function getHamsters() {
 		const response = await fetch('/hamsters', {method: 'GET'})
 		const data: Hamster[] = await response.json()
-		setHamsters(data)
-		//OBS! Bättre att hämta data i APP-komponenten, eftersom den alltid är MOUNTED.
-	
+		setHamsters(data)	
 	}
 
 	async function removeHamster(id:string) {
 		await fetch("/hamsters/" + id, { method: 'DELETE' })
 		window.location.reload()
-		// setHamsters (await getHamsters())
 	}
 
-
-	
 	return (
 		<div className="hamster-container">
-			
-		{hamsters ? hamsters.map(h =>(	
-			<div className="hamstercard" key={h.id}>
-				{/* ifsats? */}
-				{(h.imgName.startsWith('http')) ? <img src={h.imgName}alt="hamster" />: <img src={`img/${h.imgName}`}alt="hamster" />}
-				{ /* <img src={`img/${h.imgName}`}alt="hamster" />
-				<img src={h.imgName}alt="hamster" /> */}
-				<h2>{h.name}</h2><br/>
-					<button onClick={() => removeHamster(h.id)}>Remove</button>
+			{hamsters ? 
+			hamsters.map(h =>(	
+			<section className="hamstercard" key={h.id}>
+				<button onClick={() => removeHamster(h.id)}>
+					Remove Hamster
+				</button>
+				{(h.imgName.startsWith('http')) ? 
+				<img src={h.imgName}alt="hamster" />
+				: <img src={`img/${h.imgName}`}alt="hamster" />}
+				<h3>
+					{h.name}
+				</h3>
 				<HamsterInfo hamster ={h}/>
-			</div>
-			)) : 'Hämtar hamstrar från API'}
-			</div>
-			
+			</section>)) 
+			: 'Hämtar hamstrar från API'}
+		</div>	
 	)
 }
 
